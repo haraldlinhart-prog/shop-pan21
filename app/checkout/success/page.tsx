e38507c8e6e6508f78ecb4a06022d025a60fb521
haraldlinhart-prog <haraldlinhart@gmail.com>
2026-06-21 21:11:33 +0700

    Fix globals.css import in success page

--- app/checkout/success/page.tsx
+++ app/checkout/success/page.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
-import '../globals.css'
+import '@/app/globals.css'
 
 function SuccessContent() {
   const params = useSearchParams()
