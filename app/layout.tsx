33ea4a942c92c7603f181079edcd29231e2ccba7
haraldlinhart-prog <haraldlinhart@gmail.com>
2026-06-24 17:13:32 +0700

    Add: Statcounter tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           "hasMap": "https://pan21.com",
         })}} />
         <meta name="ai-crawlers" content="allowed" />
+              <script dangerouslySetInnerHTML={{__html: `var sc_project=13317697;var sc_invisible=1;var sc_security="458f783c";`}} />
+        <script async src="https://www.statcounter.com/counter/counter.js" />
       </head>
       <body>{children}</body>
     </html>
