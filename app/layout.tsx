aa7fd326964dd0a0a174297d609baf39e4c55396
haraldlinhart-prog <haraldlinhart@gmail.com>
2026-07-22 14:14:35 +0000

    fix: remove Matomo requireConsent (was blocking tracking without functioning consent trigger)

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           rel="stylesheet"
         />
         {/* Matomo 93 */}
-        <script dangerouslySetInnerHTML={{ __html: `var _paq=window._paq=window._paq||[];_paq.push(['requireConsent']);_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function(){var u="//counter.ixan.org/";_paq.push(['setTrackerUrl',u+'matomo.php']);_paq.push(['setSiteId','93']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.async=true;g.src=u+'matomo.js';s.parentNode.insertBefore(g,s);})();` }} />
         {/* Schema.org */}
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
           "@context": "https://schema.org",
